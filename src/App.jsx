272234f6b4c4bd320c1295a79a0c0f3edf57ed06
LeraB17 <bvaleria_a2002@mail.ru>
2024-04-17 00:26:40 +0500

    add UserList

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import CountdownTimer from '#components/CountdownTimer/CountdownTimer'
+import UserList from '#components/UserList/UserList'
 
 function App() {
   return (
     <div className='container'>
       <CountdownTimer />
+      <UserList />
     </div>
   )
 }
---
+++ src/components/UserList/UserList.jsx
@@ -0,0 +1,55 @@
+import React, { useEffect, useState } from 'react';
+import './UserList.css';
+
+const UserList = () => {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    useEffect(() => {
+
+        const getData = async () => {
+            setLoading(true);
+            // имитация долгой загрузки
+            const now = Date.now();
+            const timer = setInterval(async () => {
+                try {
+                    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+                    if (!response.ok) {
+                        throw new Error(`HTTP error: ${response.status}`)
+                    }
+                    const data = await response.json();
+                    setUsers(data);
+                }
+                catch (error) {
+                    console.error('Ошибка получения данных:', error);
+                } finally {
+                    setLoading(false);
+                }
+
+                if (Date.now() - now > 3000) {
+                    clearInterval(timer);
+                }
+            }, 3000)
+        }
+        getData();
+        return () => {
+            setUsers([]);
+            clearInterval(timer);
+        }
+    }, [])
+
+    return (
+        <ul className='user__list'>
+            <div>{loading}</div>
+            {
+                loading ?
+                    <div>loading...</div>
+                    : (
+                        users.map(user => <li key={user?.id}>{user?.username}</li>)
+                    )
+            }
+        </ul>
+    );
+};
+
+export default UserList;
